e715454ea824728b8f9bdaaa4ed06e81c6674c1b
Marin Aleksey <asmadews@gmail.com>
2019-04-14 13:52:00 +0300

    Добавлены шаблоны для выгрузки в md и txt

--- backup.py
+++ backup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 import os
 
 import pickle
 import html
+from datetime import date
 from datetime import datetime
 from jinja2 import Template
 from zipfile import ZipFile, ZIP_DEFLATED
 
+check_days = [
+    date(year=2010, month=3, day=6),
+    date(year=2009, month=10, day=16),
+]
 
 
 class LjRecord(object):
@@ -75,8 +78,31 @@ class LjRecord(object):
             )
 
     @property
+    def posting_date(self):
+        if isinstance(self.data['eventtime'], str):
+            evt_datetime = datetime.strptime(
+                self.data['eventtime'],
+                '%Y-%m-%d %H:%M:%S'
+            )
+            return evt_datetime.date()
+        else:
+            raise RuntimeError(
+                "Unexpected eventtime %s" % type(
+                    self.data['eventtime']
+                ).__name__
+            )
+
+
+    @property
     def url(self):
         return self.data['url']
+
+    def __str__(self):
+        _str = self.posting_datetime_string.strftime("%d.%m.%Y")
+        _str += ':' + self.subject
+        _str += f'\n{self.content}'
+        _str += f'\nТэги: {self.tags}'
+        return _str
 
 
 with open('/Users/amarin/dev/python-lj/kangaroo_mouse.pkl', 'rb') as pd:
@@ -98,25 +124,47 @@ v = entries_with_tags[1]
 # print("Anum: ", v.data['anum'], '/', v.data['itemid'])
 # print(v.data['props'])
 
-print("Loading template")
-template_path = os.path.join(
-    os.path.dirname(__file__), 'template.html'
-)
-template_tgt = os.path.join(
-    os.path.dirname(__file__), 'document.html'
-)
 
-with open(template_path, 'rb') as template_fh:
-    template = Template(template_fh.read().decode('utf-8'))
-
-output_text = template.render(posts=entries_with_tags)
-
-with open(template_tgt, 'wb') as output_fh:
-    output_fh.write(output_text.encode())
+def check_records(day: date):
+    selected = [
+        x for x in entries if x.posting_date == day
+    ]
+    for post in selected:
+        print(f'\n---------------------------\n{post}')
 
 
+def render_template(template_name, render_to):
+    print(f"Render template {template_name} into {render_to}")
+    template_path = os.path.join(
+        os.path.dirname(__file__), template_name
+    )
+    template_tgt = os.path.join(
+        os.path.dirname(__file__),
+        'render',
+        render_to
+    )
+
+    with open(template_path, 'rb') as template_fh:
+        template = Template(template_fh.read().decode('utf-8'))
+
+    output_text = template.render(posts=entries)
+    with open(template_tgt, 'wb') as output_fh:
+        output_fh.write(output_text.encode())
 
 
+def make_html():
+    render_template('template.html', 'document.html')
 
-# for k, v in v['props'].items():
-#     print("%s %s" % (k, type(v)))
+
+def make_md():
+    render_template('template.md', 'document.md')
+
+
+def make_text():
+    render_template('template.txt', 'document.txt')
+
+
+if __name__== '__main__':
+    # make_html()
+    make_md()
+    # make_text()
